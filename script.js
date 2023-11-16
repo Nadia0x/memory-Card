b540a43eaad4b6d76bac4c8a9d3af6489b5af87f
raghadaljuhani <150162020+raghadaljuhani@users.noreply.github.com>
2023-11-16 20:14:19 +0300

    Update script.js

--- script.js
+++ script.js
@@ -41,3 +41,47 @@ function initializeGame() {
     const shuffledCards = shuffle([...drawings, ...drawings.slice(0, drawings.length / 7)]);
     let flippedCards = [];
     let isProcessing = false;
+
+const gameBoard = document.getElementById('game-board');
+    const chancesLeftElement = document.getElementById('chances-left');
+    updateChancesDisplay();
+
+    function updateChancesDisplay() {
+        chancesLeftElement.textContent = `Chances left: ${chancesLeft}`;
+    }
+
+    shuffledCards.forEach((card, index) => {
+        const cardElement = document.createElement('div');
+        cardElement.classList.add('card');
+        cardElement.dataset.card = card;
+        cardElement.addEventListener('click', flipCard);
+
+        gameBoard.appendChild(cardElement);
+    });
+
+    document.getElementById('start-button').addEventListener('click', startGame);
+
+    function startGame() {
+        document.getElementById('start-button').style.display = 'none';
+        document.getElementById('game-board').style.display = 'grid';
+        document.getElementById('background-music').play(); // Play background music
+        document.getElementById('background-music').volume = 0.1;
+    }
+
+    function flipCard() {
+        if (isGameOver || isProcessing || flippedCards.length >= 2 || this.classList.contains('hidden') || this.classList.contains('flipped')) {
+            return;
+        }
+
+        document.getElementById('flip-sound').play(); // Play flip sound
+
+        this.textContent = this.dataset.card;
+        this.style.backgroundColor = '#ffd700';
+        this.classList.add('flipped');
+        flippedCards.push(this);
+
+        if (flippedCards.length === 2) {
+            isProcessing = true;
+            setTimeout(checkMatch, 500);
+        }
+    }
